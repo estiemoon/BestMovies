fdd5849a1e05594dad972341df467faffaf101ae
inhwa <dlsghk0711@naver.com>
2024-07-20 03:48:05 +0900

    tmbd API 사용

--- src/componets/BoardList/BoardList.tsx
+++ src/componets/BoardList/BoardList.tsx
@@ -1,11 +1,12 @@
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import { useTypedDispatch, useTypedSelector } from '../../hooks/redux';
 import { SideForm } from './SideForm/SideForm';
 import { addSection, boardItem, boardItemActive, container, customLink, loginBox, loginBox_container, title } from './BoardList.css';
 import clsx from 'clsx';
 import { IList, IMovieList } from '../../types';
 import { Link } from "react-router-dom";
-import { addAwardsBoard, addBookMarkBoard } from '../../store/slices/boardSlice';
+import { addAwardsBoard, addBookMarkBoard, addMovieBoard } from '../../store/slices/boardSlice';
+import { fetchMovieList } from '../MovieAPI/useFetchData';
 
 type TBoardListProps = {
   activeBoardId: string;
@@ -15,62 +16,25 @@ type TBoardListProps = {
 export const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => {
   const { boardArray } = useTypedSelector(state => state.boards);
   const inputRef = useRef<HTMLInputElement>(null);
-  const [filteredMovies, setFilteredMovies] = useState<IMovieList[]>([]);
   const dispatch = useTypedDispatch();
   const initialAccessToken = localStorage.getItem('accessToken');
 
-  const handleSearch = (searchText: string) => {
-    const boardZero = boardArray.find(board => board.boardId === 'board-0');
-    if (boardZero) {
-      const filtered = boardZero.lists.flatMap(list => list.movieList.filter(movie =>
-        movie.movName.toLowerCase().includes(searchText.toLowerCase())
-      ));
-      setFilteredMovies(filtered);
-    } else {
-      setFilteredMovies([]);
-    }
-  }
-
-  const handleClearMovie = (movId: string) => {
-    const updatedMovies = filteredMovies.filter(movie => movie.movId !== movId);
-    setFilteredMovies(updatedMovies);
-  }
-
   useEffect(() => {
-    // activeBoardId가 'board-1'일 때만 데이터를 가져오도록 설정
-    if (activeBoardId === 'board-1') {
+    if (activeBoardId === 'board-1') { // 수상작 선택 시
       fetch('http://localhost:3000/awards')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Failed to fetch data');
-          }
-          return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-          console.log('Fetched data:', data);
-
-          // 전체 데이터 배열을 순회하며 Redux 상태를 업데이트
-          data.forEach((award: { id: any; title: any; detail: any; img: any; }) => {
-            const movieToAdd = {
-              movId: award.id,
-              movName: award.title,
-              movDes: award.detail,
-              movImg: award.img,
-              bookmarked: false
-            };
-            // DB 연결하면 굳이 dispatch로 보드에 추가 안 하고 바로 출력하면 됨 
-            const existingMovie = boardArray[1].lists[0].movieList.find(movie => movie.movId === movieToAdd.movId);
-            if (!existingMovie) {
-              dispatch(addAwardsBoard(movieToAdd));
-            }
-          });
+          const moviesToAdd = data.map((award: { id: any; title: any; detail: any; img: any; }) => ({
+            movId: award.id,
+            movName: award.title,
+            movDes: award.detail,
+            movImg: award.img,
+            bookmarked: false
+          }));
+          dispatch(addAwardsBoard(moviesToAdd));
         })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-    }
-
-    else if (activeBoardId === 'board-2') {
+        .catch(error => console.error('Error fetching data:', error));
+    } else if (activeBoardId === 'board-2') { // 즐겨찾기 선택 시
       fetch('http://localhost:3000/booklist', {
         method: 'GET',
         headers: {
@@ -78,35 +42,20 @@ export const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId
           'Authorization': `Bearer ${initialAccessToken}`
         }
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Failed to fetch data');
-          }
-          return response.json();
+        .then(response => response.json())
+        .then(data => {
+          const moviesToAdd = data.map((award: { id: any; title: any; detail: any; img: any; }) => ({
+            movId: award.id,
+            movName: award.title,
+            movDes: award.detail,
+            movImg: award.img,
+            bookmarked: true
+          }));
+          dispatch(addBookMarkBoard(moviesToAdd));
         })
-        .then((data) => {
-          data.forEach((award: { id: any; title: any; detail: any; img: any; }) => {
-            const movieToAdd = {
-              movId: award.id,
-              movName: award.title,
-              movDes: award.detail,
-              movImg: award.img,
-              bookmarked: true
-            };
-            // DB 연결하면 굳이 dispatch로 보드에 추가 안 하고 바로 출력하면 됨 
-            const existingMovie = boardArray[2].lists[0].movieList.find(movie => movie.movId === movieToAdd.movId);
-            if (!existingMovie) {
-              dispatch(addBookMarkBoard(movieToAdd));
-            }
-          });
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-
-        })
+        .catch(error => console.error('Error fetching data:', error));
     }
-  }, [activeBoardId, boardArray, dispatch]);
-
+  }, [activeBoardId, dispatch, initialAccessToken]);
 
   return (
     <div className={container}>
@@ -121,17 +70,14 @@ export const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId
           </div>
         </div>
       </div>
-      {boardArray.map((board, index) => (
+      {boardArray.map((board) => (
         <div
           key={board.boardId}
           onClick={() => setActiveBoardId(board.boardId)}
-          className={
-            clsx({
-              [boardItemActive]: board.boardId === activeBoardId
-            }, {
-              [boardItem]: board.boardId !== activeBoardId
-            })
-          }
+          className={clsx({
+            [boardItemActive]: board.boardId === activeBoardId,
+            [boardItem]: board.boardId !== activeBoardId
+          })}
         >
           <div>
             {board.boardName}
@@ -142,9 +88,9 @@ export const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId
       <div className={addSection}>
         <SideForm
           inputRef={inputRef}
-          onSearch={handleSearch}
-          filteredMovies={filteredMovies}
-          onClearMovie={handleClearMovie}
+          onSearch={() => { }}
+          filteredMovies={[]} // 필터링된 영화 목록 상태가 필요하지 않다면 빈 배열을 전달
+          onClearMovie={() => { }}
         />
       </div>
     </div>
